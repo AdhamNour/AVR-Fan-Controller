179be86980805ed5e2f4e821f8e097acad77f17e
Adham Nour <adhamnourelwaffaa@gmail.com>
2023-10-21 19:45:20 +0300

    finished displaying temp on LCD

--- Application/Application.c
+++ Application/Application.c
@@ -9,6 +9,7 @@
 
 #include <util/delay.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "../HAL/DC_Motor/DC_Motor.h"
 #include "../HAL/LCD/LCD.h"
@@ -16,20 +17,29 @@
 
 static void Application_init();
 static void Application_splashScreen();
+static void Application_readTempreture();
+static void Application_printTempreture();
+
+static uint8 current_temp=0;
+static uint8 tempreature_string[3];
+static uint8 displayString[16];
 
 void Application_Setup() {
+	static  uint8 tempString[] = "Temp =     C";
+	static  uint8 fanString[] = "Fan is OFF";
 	Application_init();
 	Application_splashScreen();
+	LCD_displayStringRowColumn(0,3,fanString);
+
+	LCD_displayStringRowColumn(1,2,tempString);
 
 }
 
 void Application_Loop() {
-	LCD_clearScreen();
-	uint8 x = LM35_readTemp();
-	uint8 a[16];
-	itoa(x, a, 10);
-	LCD_displayString(a);
-	_delay_ms(1000);
+	Application_readTempreture();
+	Application_printTempreture();
+
+
 }
 
 static void Application_init(){
@@ -50,3 +60,16 @@ static void Application_splashScreen(){
 
 	LCD_clearScreen();
 }
+
+
+static void Application_readTempreture(){
+	current_temp = LM35_readTemp();
+
+}
+static void Application_printTempreture(){
+	itoa(current_temp,tempreature_string,10);
+
+	LCD_displayStringRowColumn(1,9,tempreature_string);
+	LCD_displayString("  ");
+}
+

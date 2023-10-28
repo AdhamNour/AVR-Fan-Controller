16b8a5d7fd02e41f9aa8ed9bde7a95c97a7e10f8
Adham Nour <adhamnourelwaffaa@gmail.com>
2023-10-28 12:36:44 +0200

    finished

---
+++ Debug/Application/Application.d
@@ -0,0 +1,37 @@
+Application/Application.o Application/Application.o:  \
+ ../Application/Application.c ../Application/Application.h \
+  ../Application/../HAL/DC_Motor/DC_Motor.h \
+  ../Application/../HAL/DC_Motor/../../MCAL/GPIO/gpio.h \
+  ../Application/../HAL/DC_Motor/../../MCAL/GPIO/../../std_types.h \
+  ../Application/../HAL/DC_Motor/../../MCAL/PWM_Timer0/PWM_Timer0.h \
+  ../Application/../HAL/DC_Motor/../../MCAL/PWM_Timer0/../../std_types.h \
+  ../Application/../HAL/LCD/LCD.h \
+  ../Application/../HAL/LCD/../../std_types.h \
+  ../Application/../HAL/LCD/../../MCAL/GPIO/gpio.h \
+  ../Application/../HAL/LM35/LM35.h \
+  ../Application/../HAL/LM35/../../MCAL/ADC/ADC.h \
+  ../Application/../HAL/LM35/../../MCAL/ADC/../../std_types.h
+
+../Application/Application.h:
+
+../Application/../HAL/DC_Motor/DC_Motor.h:
+
+../Application/../HAL/DC_Motor/../../MCAL/GPIO/gpio.h:
+
+../Application/../HAL/DC_Motor/../../MCAL/GPIO/../../std_types.h:
+
+../Application/../HAL/DC_Motor/../../MCAL/PWM_Timer0/PWM_Timer0.h:
+
+../Application/../HAL/DC_Motor/../../MCAL/PWM_Timer0/../../std_types.h:
+
+../Application/../HAL/LCD/LCD.h:
+
+../Application/../HAL/LCD/../../std_types.h:
+
+../Application/../HAL/LCD/../../MCAL/GPIO/gpio.h:
+
+../Application/../HAL/LM35/LM35.h:
+
+../Application/../HAL/LM35/../../MCAL/ADC/ADC.h:
+
+../Application/../HAL/LM35/../../MCAL/ADC/../../std_types.h:
